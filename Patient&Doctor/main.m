db975a0c14b58130c1fb5663abb7370dd7c5600a
victorhong1988 <victorhong1988@gmail.com>
2016-11-03 10:44:52 -0400

    Initial Commit

---
+++ Patient&Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient&Doctor
+//
+//  Created by Victor Hong on 03/11/2016.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
